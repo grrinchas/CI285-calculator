1310ab5278ff2e137697ba7e09d4f35754e52bdb
Dennis Grinch <dg4675dg@gmail.com>
2017-03-17 00:11:13 +0000

    Add js

--- static/main.js
+++ static/main.js
@@ -1,5 +1,43 @@
-$(document).ready(function(){
-    $("#p1").mousedown(function(){
-        alert("Mouse down over p1!");
+function Operation(n1, n2, op) {
+    this.n1 = n1;
+    this.n2 = n2;
+    this.op = op;
+
+    this.toPath = function () {
+
+        switch (this.op) {
+            case 'add':
+                return "/additions/" + this.n1 + "/" + this.n2;
+            case 'subtract':
+                return "/subtractions/"+ this.n1 + "/" +  this.n2;
+            case 'multiply':
+                return "/multiplications/"+ this.n1 + "/" + this.n2;
+            case 'divide':
+                return "/divisions/"+ this.n1 + "/" + this.n2;
+            default:
+                throw new Error('operation not supported: ' + this.op)
+        }
+    };
+}
+
+function getPath() {
+    var localhost = "http://localhost:3000"
+    var operation = new Operation($('#first-operand').val(), $('#second-operand').val(), $('#operations').val()).toPath();
+    return localhost + operation;
+}
+
+
+$('#calculator').submit(function (event) {
+
+    jQuery.ajax( {
+        type: "GET",
+        url: getPath(),
+        dataType: "json",
+        success: function (data, status, req) {
+            $('#answer').text(JSON.parse(data).result);
+        }
     });
+
+    event.preventDefault();
 });
+
